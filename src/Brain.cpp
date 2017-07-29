1f3b873adf9da81fbfee450d314a7545fa8e74ea
dy <dengyong.ai@gmail.com>
2017-07-29 16:05:50 +0900

    update

--- src/Brain.cpp
+++ src/Brain.cpp
@@ -292,7 +292,7 @@ Brain::less36_decesion_tree(const PredictState & state,const ActionStatePair pat
 
     const double targetBall_dist_goal =targetBall.dist(ServerParam::i().theirTeamGoalPos());
 
-    int spent_time= 0;  /*first_action.category()==CooperativeAction::Dribble?first_action.durationStep():0;*/
+    int spent_time=first_action.category()==CooperativeAction::Dribble?first_action.durationStep():0;
 
     double  opp_dist_target_ball = FieldAnalyzer::get_opponent_dist(state,targetBall,spent_time,3);
     double  opp_dist_current_ball= FieldAnalyzer::get_opponent_dist(state,currentBall,0,3);
@@ -318,7 +318,7 @@ Brain::less36_decesion_tree(const PredictState & state,const ActionStatePair pat
     }
 
 
-    bool  over_defensive_line=state.ball().pos().x>state.theirDefensePlayerLineX()-1.0? true:false;
+    bool  over_defensive_line=state.ball().pos().x>state.theirDefensePlayerLineX()? true:false;
 
 
     if (safe) //0.0
@@ -359,7 +359,7 @@ Brain::less36_decesion_tree(const PredictState & state,const ActionStatePair pat
     {
 
 
-        double  basic_try_safe_bonus=bound(0.0, opp_dist_target_ball,4.0)*0.25;
+        double  basic_try_safe_bonus=bound(0.0, opp_dist_target_ball,4.0)*2.5;
 
         p+=basic_try_safe_bonus;
 
@@ -489,72 +489,4 @@ Brain::over36_decesion_tree(const PredictState & state,const ActionStatePair pat
 
 }
 
-
-
-
-
-bool
-Brain::is_bed_pass(const ActionStatePair path)
-{
-//    const CooperativeAction  &first_action =path.action();
-//    const PredictState & state=path.state();
-
-//    double basic_eva=evaluate_state(path.state());
-
-
-
-//    const Vector2D  receive_pos=state.ball().pos();
-//    const Vector2D  first_ball_pos=state.current_wm()->ball().pos();
-
-//    static const double CONTROL_AREA_BUF = 0.15;  // buffer for kick table
-
-//    const ServerParam & SP = ServerParam::i();
-
-
-
-//    const AbstractPlayerCont::const_iterator end = state.theirPlayers().end();
-//    for ( AbstractPlayerCont::const_iterator o = state.theirPlayers().begin();
-//          o != end;
-//          ++o )
-//    {
-
-//        if (!(*o)) continue;
-
-//        const Vector2D opponent_pos=(*o)->pos();
-
-//        if (opponent_pos.x<35.0) continue;
-
-//          double max_x=std::max(first_ball_pos.x,receive_pos.x);
-//          double min_x=std::min(first_ball_pos.x,receive_pos.x);
-//          //4.0 as  a buff
-
-//          if (opponent_pos.x>max_x) continue;
-//          if (opponent_pos.x<min_x) continue;
-
-//          double max_y=std::max(first_ball_pos.y,receive_pos.y);
-//          double min_y=std::min(first_ball_pos.y,receive_pos.y);
-
-//          if ( opponent_pos.y>max_y)  continue;
-//          if ( opponent_pos.y<min_y)  continue;
-
-
-//          // pass line
-//           Segment2D passLine(first_ball_pos,receive_pos);
-
-//         if (passLine.contains(opponent_pos) ||passLine.dist( opponent_pos)<1.2)
-//         {
-
-//            return true;
-//         }
-
-
-
-//      }
-
-
-
-
-
-    return false;
-}
 
